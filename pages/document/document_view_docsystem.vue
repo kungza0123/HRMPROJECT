b39669ada7240f7d1dbfe62adddf879ab5aff025
kungza0123 <6331305002@lamduan.mfu.ac.th>
2024-01-29 14:51:05 +0700

    fff

---
+++ pages/document/document_view_docsystem.vue
@@ -0,0 +1,180 @@
+<template>
+  <div>
+    <div class="text-center d-flex align-center justify-center">
+      <h3>พนักงานทั้งหมด</h3>
+    </div>
+    <v-card>
+      <v-flex xs12 sm6 md3>
+        <v-card-title>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        
+      </v-flex>
+      
+      <v-data-table :headers="headers" :items="desserts" :search="search"></v-data-table>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      search: "",
+      headers: [
+        {
+          text: "รหัสพนักงาน",
+          align: "start",
+          filterable: false,
+          value: "iduser",
+        },
+        { text: "ชื่อ-นามสกุล", value: "namelastname" },
+        { text: "เพศ", value: "gender" },
+        { text: "Email", value: "email" },
+        { text: "แผนก ", value: "department" },
+        { text: "ฝ่าย ", value: "department1" },
+        { text: "ตำแหน่ง ", value: "department2" },
+        { text: "สถานะ ", value: "department3" },
+        { text: "edit-delete", value: "" },
+      ],
+      desserts: [
+        {
+          iduser: "6250",
+          namelastname: "Frozen Yogurt",
+          gender: "ชาย",
+          email: "HARAJUKU@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+        {
+          iduser: "6251",
+          namelastname: "Gwendoline Sylvestre",
+          gender: "ชาย",
+          email: "GwendolineSylvestre@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+        {
+          iduser: "6252",
+          namelastname: "Everard",
+          gender: "ชาย",
+          email: "Everard@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+        {
+          iduser: "6253",
+          namelastname: "Frozen Yogurt",
+          gender: "ชาย",
+          email: "HARAJUKU@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+        {
+          iduser: "6254",
+          namelastname: "Frozen Yogurt",
+          gender: "ชาย",
+          email: "HARAJUKU@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+        {
+          iduser: "6255",
+          namelastname: "Frozen Yogurt",
+          gender: "ชาย",
+          email: "HARAJUKU@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+        {
+          iduser: "6256",
+          namelastname: "Frozen Yogurt",
+          gender: "ชาย",
+          email: "HARAJUKU@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+        {
+          iduser: "6257",
+          namelastname: "Frozen Yogurt",
+          gender: "ชาย",
+          email: "HARAJUKU@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+        {
+          iduser: "6258",
+          namelastname: "Frozen Yogurt",
+          gender: "ชาย",
+          email: "HARAJUKU@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+        {
+          iduser: "6259",
+          namelastname: "Frozen Yogurt",
+          gender: "ชาย",
+          email: "HARAJUKU@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+        {
+          iduser: "6260",
+          namelastname: "Frozen Yogurt",
+          gender: "ชาย",
+          email: "HARAJUKU@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+        {
+          iduser: "6261",
+          namelastname: "Frozen Yogurt",
+          gender: "ชาย",
+          email: "HARAJUKU@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+        {
+          iduser: "6262",
+          namelastname: "kungzaza",
+          gender: "ชาย",
+          email: "HARAJUKU@GMAIL.COM",
+          department: "sdadasdasd",
+          department1: "sdasdasdasd",
+          department2: "adasdasdasdas",
+          department3: "กำลังทำงาน",
+        },
+      ],
+    };
+  },
+};
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,67 +1,100 @@
 <template>
-    <v-row class="text-center d-flex align-center justify-center">
-        <v-col cols="12" sm="8" md="6">
+  <v-row class="text-center d-flex align-center justify-center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <div class="container">
+          <img class="d-flex justify-center" src="@/assets/logo.png" alt="Vue.js Logo" />
+        </div>
 
-            <v-card>
-                <div class="container">
-                    <img class="d-flex justify-center" src="@/assets/logo.png" alt="Vue.js Logo" />
-                </div>
+        <v-card-title class="headline justify-center"> LOGIN </v-card-title>
+        <v-card-text>
+          <v-sheet width="300" class="mx-auto">
+            <v-form fast-fail @submit.prevent>
+              <v-text-field
+                v-model="Username"
+                label="Username"
+                :rules="UsernameRules"
+              ></v-text-field>
 
-                <v-card-title class="headline justify-center">
-                    LOGIN
-                </v-card-title>
-                <v-card-text>
-                    <v-sheet width="300" class="mx-auto">
-                        <v-form fast-fail @submit.prevent>
-                            <v-text-field v-model="Username" label="Username" :rules="UsernameRules"></v-text-field>
+              <v-text-field
+                v-model="Password"
+                label="Password"
+                :rules="PasswordRules"
+              ></v-text-field>
 
-                            <v-text-field v-model="Password" label="Password" :rules="PasswordRules"></v-text-field>
-
-                            <v-btn type="submit" block class="mt-2">Login</v-btn>
-                        </v-form>
-                    </v-sheet>
-
-                </v-card-text>
-                <v-card-actions>
-                </v-card-actions>
-            </v-card>
-        </v-col>
-    </v-row>
+              <v-btn type="submit" block class="mt-2" to="./document/document_view_docsystem">Login</v-btn>
+            </v-form>
+          </v-sheet>
+        </v-card-text>
+        <v-card-actions> </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
-  
+
 <script>
+import { validationMixin } from "vuelidate";
+import { required, maxLength, email } from "vuelidate/lib/validators";
 export default {
-    data: () => ({
-        Username: '',
-        UsernameRules: [
-            value => {
-                if (value?.length > 3) return true
+  //   mixins: [validationMixin],
 
-                return 'First name must be at least 3 characters.'
-            },
-        ],
-        Password: '',
-        PasswordRules: [
-            value => {
-                if (/[^0-9]/.test(value)) return true
+  // validations: {
+  //   Username: { required, maxLength: maxLength(10) },
+  //   email: { required, email },
+  //   select: { required },
+  // },
 
-                return 'Last name can not contain digits.'
-            },
-        ],
-    }),
-}
+  data: () => ({
+    Username: "",
+    UsernameRules: [
+      (value) => {
+        if (value?.length > 3) return true;
+
+        return "Usernsme is required.";
+      },
+    ],
+    Password: "",
+    PasswordRules: [
+      (value) => {
+        if (/[^0-9]/.test(value)) return true;
+
+        return "Password is required.";
+      },
+    ],
+  }),
+
+  // UsernameErrors () {
+  //       const errors = []
+  //       if (!this.$v.Usernamename.$dirty) return errors
+  //       // !this.$v.Usernamename.maxLength && errors.push('Name must be at most 10 characters long')
+  //       !this.$v.Usernamename.required && errors.push('Username is required.')
+  //       return errors
+  //     },
+  // PasswordErrors () {
+  //       const errors = []
+  //       if (!this.$v.Password.$dirty) return errors
+  //       // !this.$v.Password.maxLength && errors.push('Name must be at most 10 characters long')
+  //       !this.$v.Password.required && errors.push('Password is required.')
+  //       return errors
+  //     },
+
+  methods: () => ({
+    submit(){
+      
+    }
+  }),
+};
 </script>
 <style scoped>
 .container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 img {
-    width: 200px;
-    /* ปรับขนาดของรูปภาพตามต้องการ */
-    height: auto;
+  width: 200px;
+  /* ปรับขนาดของรูปภาพตามต้องการ */
+  height: auto;
 }
 </style>
