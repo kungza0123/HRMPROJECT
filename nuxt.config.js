2b6d08fa664b0e6e0596d7635de4edebf6b2e47c
kungza0123 <6331305002@lamduan.mfu.ac.th>
2024-02-06 14:22:02 +0700

    six

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: './assets/S8.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
