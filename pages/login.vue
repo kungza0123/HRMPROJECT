73f5b8bd9d4e427d405dc7841e1d99a61aa2c116
kungza0123 <6331305002@lamduan.mfu.ac.th>
2024-02-07 11:38:56 +0700

    six

--- pages/login.vue
+++ pages/login.vue
@@ -1,62 +1,66 @@
 <template>
-  <v-row class="text-center d-flex align-center justify-center">
-    <v-col cols="12" sm="8" md="6">
-    <div class="centero">
-     <v-card >
-        <div class="container">
-          <img class="d-flex justify-center" src="@/assets/logo.png" alt="Vue.js Logo" />
-          <img class="d-flex justify-center" src="@/assets/logo.png" alt="Vue.js Logo" />
+  <div class="container">
+    <v-card class="mx-auto pa-15 pb-8" elevation="8" max-width="555" rounded="lg">
+      <div class="centero">
+        <img class="d-flex justify-center" src="@/assets/S16.png" alt="Vue.js Logo" />
+      </div>
+
+      <v-card-title class="headline justify-center">
+        <div>
+          <h3>LOGIN</h3>
         </div>
+      </v-card-title>
+      <v-card-text>
+        <v-sheet width="auto" class="mx-auto">
+          <v-form fast-fail @submit.prevent>
+            <v-text-field
+              v-model="Username"
+              label="ชื่อผู้ใช้"
+              :rules="UsernameRules"
+              prepend-inner-icon="mdi-account"
+              outlined
+            ></v-text-field>
 
-        <v-card-title class="headline justify-center">
-          <div>
-            <h3>LOGIN</h3>
-          </div>
-        </v-card-title>
-        <v-card-text>
-          <v-sheet width="300" class="mx-auto">
-            <v-form fast-fail @submit.prevent>
-              <v-text-field
-                v-model="Username"
-                label="Username"
-                :rules="UsernameRules"
-              ></v-text-field>
-
-              <v-text-field
-                v-model="Password"
-                label="Password"
-                :rules="PasswordRules"
-              ></v-text-field>
-
-              <v-btn
-                type="submit"
-                block
-                class="mt-2"
-                to="./document/document_view_docsystem"
-                >Login</v-btn
-              >
-            </v-form>
-          </v-sheet>
-        </v-card-text>
-        <v-card-actions> </v-card-actions>
-      </v-card>
-    
-    </div>
-     
-    </v-col>
-  </v-row>
+            <v-text-field
+              v-model="Password"
+              label="รหัสผ่าน"
+              :rules="PasswordRules"
+              :append-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+              :type="visible ? 'text' : 'password'"
+              density="compact"
+              prepend-inner-icon="mdi-lock-outline"
+              outlined
+              @click:append="togglePasswordVisibility"
+            ></v-text-field>
+            <v-btn
+              type="submit"
+              block
+              class="mt-2"
+              to="./document/document_view_docsystem"
+            >Login</v-btn>
+          </v-form>
+        </v-sheet>
+      </v-card-text>
+      <v-card-actions> </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
   layout: "nonnav",
+  methods: {
+    togglePasswordVisibility() {
+      this.visible = !this.visible;
+    },
+  },
   data: () => ({
     Username: "",
     UsernameRules: [
       (value) => {
         if (value?.length > 3) return true;
 
-        return "Usernsme is required.";
+        return "กรุณากรอกชื่อผู้ใช้";
       },
     ],
     Password: "",
@@ -64,33 +68,35 @@ export default {
       (value) => {
         if (/[^0-9]/.test(value)) return true;
 
-        return "Password is required.";
+        return "กรุณากรอกรหัสผ่าน";
       },
     ],
-  }),
-
-  methods: () => ({
-    submit() {},
+    visible: false,
   }),
 };
 </script>
+
 <style scoped>
 .container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 
 img {
   width: 200px;
-  /* ปรับขนาดของรูปภาพตามต้องการ */
   height: auto;
 }
-.centero{
-  position: absolute;
-   top: 20%;
-  width: 45%; 
-  
+
+.centero {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.v-btn:hover {
+  background-color: #FFB319 !important;
+  color: white !important;
 }
 </style>
